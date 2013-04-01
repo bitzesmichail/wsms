5e4c8c9e344a3cae67067de073a0f9918ad43607
alkis <alkis.kalogeris@gmail.com>
2013-04-01 23:52:02 +0300

    kwsta koita auto to entity

--- Model/entities/Customer.php
+++ Model/entities/Customer.php
@@ -12,29 +12,29 @@ class Customer
     
     public function __get($param)
     {
-	switch ($param)
-	{    
-	    case "idCustomer":
-		return $this->customer['idCustomer'];
-	    case "name":
-		return $this->customer['name'];
-	    case "surname":
-		return $this->customer['surname'];
-	    case "ssn":
-		return $this->customer['ssn'];
-	    case "phone":
-		return $this->customer['phone'];
-	    case "cellphone":
-		return $this->customer['cellphone'];
-	    case "email":
-		return $this->customer['email'];
-	    case "address":
-		return $this->customer['address'];
-	    case "zipcode":
-		return $this->customer['zipcode'];
-	    case "city":
-		return $this->customer['city'];
-	}
+		switch ($param)
+		{    
+			case "idCustomer":
+			return $this->customer['idCustomer'];
+			case "name":
+			return $this->customer['name'];
+			case "surname":
+			return $this->customer['surname'];
+			case "ssn":
+			return $this->customer['ssn'];
+			case "phone":
+			return $this->customer['phone'];
+			case "cellphone":
+			return $this->customer['cellphone'];
+			case "email":
+			return $this->customer['email'];
+			case "address":
+			return $this->customer['address'];
+			case "zipcode":
+			return $this->customer['zipcode'];
+			case "city":
+			return $this->customer['city'];
+		}
     }
 
 }
