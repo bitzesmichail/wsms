226f41bcb938d33330962dea05d9527b3abc79d0
Vangelis Kolaitis <vangelis@Ares.(none)>
2013-04-03 20:32:37 +0300

    started working on index

---
+++ View/index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+		<script src="jquery.js"></script>
+		<script src="scriptd.js"></script>
+	</head>
+	<body>
+	</body>
+</html>
