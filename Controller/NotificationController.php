4cd9ca077403ef178bbc87a66cba38c0c57a6d02
Constantine <kgiann78@gmail.com>
2013-04-08 23:35:55 +0300

    Notifications' System Preparation
    Επειδή το να ασχοληθούμε με τα orders μάλλον είναι πιο δύσκολο, είπα να
    ξεκινήσω με το notifications για να είναι και ολοκληρωμένη η υπηρεσία
    εισαγωγής χρηστών, customers, providers, κλπ...

--- Controller/NotificationController.php
+++ Controller/NotificationController.php
@@ -1,32 +1,41 @@
 <?php
 
+require_once 'Notification.php';
+require_once 'Model/NotificationModel.php';
+
 /**
  * Controller for notifications
  */
  class NotificationController extends Controller
  {
- 	function __construct(argument)
+ 	function __construct()
  	{
  		# code...
  	}
 
  	public function create($notification='')
  	{
+ 		// return NotificationModel::create($notification);
  		return 0;
  	}
 
  	public function receive()
  	{
+ 		// return NotificationModel::getNotifications();
  		return 0;
  	}
 
  	public function view($id='')
  	{
+ 		// return NotificationModel::getNotificationById($id);
  		return 0;
  	}
 
  	public function dismiss($id='')
  	{
+ 		// check for notifications that are not read
+ 		// if all read then delete
+ 		// return NotificationModel::delete($id);
  		return 0;
  	}
  }
