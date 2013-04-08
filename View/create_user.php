9850e5bc1175d8938c166018cd8d672629df95e6
nmichelis <n.michelis90@gmail.com>
2013-04-08 14:38:02 +0300

    Update create_user.php

---
+++ View/create_user.php
@@ -0,0 +1,29 @@
+ <?php
+require "vcc.php" ;
+if($loggedin)
+{
+  if($user['role'] == manager)
+	{
+		if(isset($_POST['subm']) == false)
+		{
+	?>
+	<form action="create_user.php" method="post">
+		<input type="hidden" name="subm" value="a" />
+		Όνομα χρήστη: <input type="text" name="username" /><br />
+		Κωδικός χρήστη: <input type="password" name="password" /><br />
+		Επιβεβαίωση κωδικού: <input type="password" name="password_acc" onChange="check_passwords()" /><img id="check_passwords" /><br />
+		E-mail: <input type="text" name="mail" /><br />
+		<input type="submit" value="Δημιουργία Χρήστη" />
+	</form>
+	<?php
+		}
+		else
+		{
+			$username = addslashes($_POST['username']) ;
+			$password = addslashes($_POST['password']) ;
+			$mail = addslashes($_POST['mail']) ;
+			create_user($username, $password, $mail) ;
+		}
+	}
+}
+?>
