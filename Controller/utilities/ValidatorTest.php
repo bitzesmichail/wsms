590f39ea3e8d14a83f3cd5a9a151d2d221cbb6d6
Michail Bitzes <bitzesmichail@gmail.com>
2013-04-13 22:22:30 +0300

    Add an AFM Validation function
    Add class Validator and test for it. This class will be expanded to check
    for the validaty of emails, zip codes etc.

---
+++ Controller/utilities/ValidatorTest.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'Validator.php';
+
+class ValidationTest extends PHPUnit_Framework_TestCase 
+{
+	public function testSSNValidator()
+	{
+		echo "\n";
+		$this->assertEquals(true, Validator::isValid_SSN("090000045"));
+		$this->assertEquals(false, Validator::isValid_SSN("tyropites"));
+		$this->assertEquals(true, Validator::isValid_SSN("094019245"));
+		$this->assertEquals(false, Validator::isValid_SSN("123"));
+		$this->assertEquals(false, Validator::isValid_SSN("123456789"));
+	}
+}
