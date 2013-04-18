63f1044fa3610073fcfe030d85009c47f7b17ca7
sandu <sandu@serendipity.(none)>
2013-04-18 20:13:19 +0300

    mikro test

--- View/login.php
+++ View/login.php
@@ -27,7 +27,7 @@ else
 		<div id="logInForm">
 			<form action="login.php" method="post">
 				<input type="hidden" name="subm" value="yes" />
-				Όνομα Χρήστη: <input type="text" name="username" /><br />
+				Όνομα Χρήστη:  <input type="text" name="username" /><br />
 				Κωδικός Χρήστη: <input type="password" name="password" /><br />
 				<input type="submit" value="Είσοδος στο Σύστημα" /> 
 				<a href="reset_password.php">Δεν θυμάμαι τον κωδικό μου</a>
