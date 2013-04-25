28289008c22a52f1c9982ad28ac084cb61cfd7b5
Michail Bitzes <bitzesmichail@gmail.com>
2013-04-26 01:18:45 +0300

    Yet another test for the hook

--- Controller/utilities/Validator.php
+++ Controller/utilities/Validator.php
@@ -10,7 +10,7 @@
 
  		$last = $afm[8];
 
- 		$sum = $afm[0]*256 + $afm[1]*128 + $afm[2]*64 + $afm[3]*32 + 
+ 		$sum = $afm[0]*256 + $afm[1]*128 + $afm[2]*64 + $afm[3]*32 +
  		       $afm[4]*16  + $afm[5]*8   + $afm[6]*4  + $afm[7]*2;
 
  		return (($sum % 11) % 10) == $last;
