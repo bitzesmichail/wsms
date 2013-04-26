9963c59674d36ea172535d16ed288a3f6201167b
Constantine <Constantine@Konstantinoss-MacBook-Pro.local>
2013-04-27 00:11:55 +0300

    μ οπως λέμε προμήθειες

--- Views/navbars/manager_navbar.php
+++ Views/navbars/manager_navbar.php
@@ -2,7 +2,7 @@
   <div class="navbar-inner">
     <ul class="nav">
       <li class="active"><a href="#">Παραγγελίες</a></li>
-      <li><a href="#">Προήθειες</a></li>
+      <li><a href="#">Προμήθειες</a></li>
       <li><a href="#">Πελάτες</a></li>
       <li><a href="#">Προμηθευτές</a></li>
       <li><a href="#">Προϊόντα</a></li>
