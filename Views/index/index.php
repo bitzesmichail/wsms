8c313dcf0852815d0def08ef99dc420b8410e4ac
Constantine <Constantine@Konstantinoss-MacBook-Pro.local>
2013-04-26 21:23:53 +0300

    adding menubar (it's actually a navbar xaxa)

--- Views/index/index.php
+++ Views/index/index.php
@@ -1,3 +1,13 @@
+<div class="navbar">
+  <div class="navbar-inner">
+    <a class="brand" href="#">Title</a>
+    <ul class="nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+    </ul>
+  </div>
+</div>
 <?php if (isset($_SESSION['username'])): ?>
 		<div class="span3">
           <div class="well sidebar-nav">
