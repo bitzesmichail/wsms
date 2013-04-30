6354c524b9539329c492cbae69054402c1170856
Michail Bitzes <bitzesmichail@gmail.com>
2013-04-30 15:24:11 +0300

    Προσθήκη bootstrap στο Error View

--- Views/header.php
+++ Views/header.php
@@ -1,8 +1,8 @@
 <!doctype html>
 <html>
 <head>
+  <meta charset="utf-8">
 	<title>Test</title>
-	<meta charset="utf-8">
     
     <!-- metadata -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
