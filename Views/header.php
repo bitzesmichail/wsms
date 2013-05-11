7186654593b24394aa2929be60929d22300afdae
Michail Bitzes <bitzesmichail@gmail.com>
2013-05-11 17:01:23 +0300

    mini commit... improve the title

--- Views/header.php
+++ Views/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-	<title>Test</title>
+	<title>WSMS</title>
     
     <!-- metadata -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
