ffd22517229ed39b275162fa63b4bb5eefad8345
Michail Bitzes <bitzesmichail@gmail.com>
2013-05-14 18:39:12 +0300

    Page index cleanup. Check for empty username/password

--- Views/page/index.php
+++ Views/page/index.php
@@ -1,17 +1,3 @@
-<?php if (isset($_SESSION['username'])): ?>
-		<!-- <div class="span3">
-          <div class="well sidebar-nav">
-            <ul class="nav nav-list">
-              <li class="nav-header">Users Menu</li>
-              <li class="active"><a href="<?php echo USERS; ?>/viewAll">Users</a></li>
-              <li><a href="<?php echo USERS; ?>/create">Create New User</a></li>
-              <li><a href="<?php echo USERS; ?>/viewById/1">View By Id (1)</a></li>
-            </ul>
-          </div><!--/.well -->
-        <!--</div><!--/span-->
-    	<!-- <div class="span9"> -->
-<?php endif ?>
-
 <?php
   require_once 'Views/navbars/navbar.php';
 ?>
@@ -22,10 +8,9 @@
 		<?php if (!isset($_SESSION['username'])): ?>
 			<p>Καλώς ήρθατε! Αυτή είναι η αρχική σελίδα του συστήματος διαχείρισης αποθηκών και οργάνωσης πωλήσεων. Αν δεν έχετε ήδη λογαριασμό 
 				αποτανθείτε στον manager προκειμένου να δημιουργήσει ένα για λογαριασμό σας.
-			<!-- <p><a href="#" class="btn btn-primary btn-large">Εγγραφή »</a></p> -->
-            	<form class="navbar-form pull-right" method="post" action="<?php echo USERS; ?>/login">
-            		<input class="span2" name="username" type="text" placeholder="Username">
-                	<input class="span2" name="password" type="password" placeholder="Password">
+            	<form class="navbar-form pull-right" onsubmit="return check()" method="post" action="<?php echo USERS; ?>/login">
+            		<input class="span2" id="username" name="username" type="text" placeholder="Username">
+                	<input class="span2" id="password" name="password" type="password" placeholder="Password">
               		<button type="submit" class="btn">Είσοδος</button>
 	            </form>
 	        <?php else: ?>
@@ -34,3 +19,15 @@
             <?php endif ?>
 	</p>
 </div>
+
+<script type="text/javascript">
+//check if username/password fields are empty
+function check() {
+  if(document.getElementById("username").value == "" || document.getElementById("password").value == "") 
+  {
+      alert("Κενό username ή password");
+      return false;
+  }
+    return true;
+}
+</script>
