455de4aa5b12fff51fc72dc30596825dc966315b
sanduG <sandy.ganoti@gmail.com>
2013-05-14 20:19:43 +0300

    update supplies layout

--- Views/supplies/index.php
+++ Views/supplies/index.php
@@ -3,7 +3,9 @@
   require_once 'Views/navbars/navbar.php';
 ?>
 
+<div class="container">
 <div class="hero-unit">
-	<h3>Προμήθειες</h3>
+	<h2>Προμήθειες</h2>
 	<p>Αυτή είναι η κύρια σελίδα προμηθειών</p>
+</div>
 </div>
