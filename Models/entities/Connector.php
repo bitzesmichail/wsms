c1d5dcf140b44206b30a13c8f34606a8440c15ea
sanduG <sandy.ganoti@gmail.com>
2013-05-19 22:29:40 +0300

    merged file problem

--- Models/entities/Connector.php
+++ Models/entities/Connector.php
@@ -15,8 +15,8 @@ class Connector
         try
         {
             $dsn = "mysql:host=localhost;dbname=wsms;charset=UTF8";
-            $username = "root";
-            $password = "Manos9387";
+            $username = "wsms";
+            $password = "mia_oraia_petalouda";
         
             $this->pdo = new PDO($dsn, $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         } 
