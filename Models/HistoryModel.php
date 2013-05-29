1e8440557b8d68c91918add2b46810232383b45f
alkis <alkis.kalogeris@gmail.com>
2013-05-29 09:45:11 +0300

    path gia phpexcel
    ebala ena path pou nomizw einai to swsto. alla prepei na to dei o
    mixalhs gia sigouria. an to balei se kapoio staging 8a prepei na mpei
    allo ena ../ giati auta exoun ena ba8os parapanw

--- Models/HistoryModel.php
+++ Models/HistoryModel.php
@@ -9,7 +9,7 @@ require_once("entities/ProductStatistics.php");
 require_once("entities/MiddleProduct.php");
 require_once("entities/HistorySaleOrder.php");
 require_once("entities/HistorySupplyOrder.php");
-
+require_once ("../../phpexcel/Classes/PHPExcel.php");
 
 class HistoryModel extends Model
 {
